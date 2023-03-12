d2dcb0598ef57b0196cb6cf90a93a636186a2e92
kireev <kireevmw@yandex.ru>
2023-03-12 23:13:09 +0700

    add starter

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row justify="center">
+    <v-col class="mx-6">
+      <p>index page</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
---
+++ pages/somePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <p>some page</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'SomePage'
+}
+</script>
